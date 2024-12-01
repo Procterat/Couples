afcb46c22d64981744ec5743620e08953c041988
Procterat <102568697+Procterat@users.noreply.github.com>
2024-12-01 17:16:16 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -34,15 +34,48 @@ function createBoard(event) {
     game.appendChild(template);
 
     let count = numberOfElements ** 2;
-    for (let i = 0; i < count; i++) {
-        table.appendChild(createCard());
-    }
+
+    let icons = createIconsArray();
+    console.log(icons);
+
+    icons.forEach((icon) => {
+        table.appendChild(createCard(icon));
+    });
 }
 
-function createCard() {
+function createCard(flippedIcon) {
     const cardTemplate = document.querySelector('#cardTemplate');
     const template = cardTemplate.content.cloneNode(true);
     const card = template.querySelector('.card');
+    card.querySelector('#flippedIcon').classList.add(`fa-${flippedIcon}`);
 
     return card;
+}
+
+function createIconsArray() {
+    let cardsIcons = ["compass", "cloud", "play", "bolt", "stop", "cogs", "atom", "basketball-ball", "arrows", "angle-left", "bars", "file", "filter", "gear", "folder", "folder-open", "shield", "scissors", "pen-clip"];
+    let needIcons;
+
+    if (numberOfElements == 2) {
+        needIcons = cardsIcons.slice(0, numberOfElements * 1);
+    } else if (numberOfElements == 4) {
+        needIcons = cardsIcons.slice(0, numberOfElements * 2);
+    } else if (numberOfElements == 6) {
+        needIcons = cardsIcons.slice(0, numberOfElements * 3);
+    }
+    
+    
+    let doubleCards = dublicateElements(needIcons);
+    let result = doubleCards.sort(() => Math.random() - 0.5);
+
+    return result;
+}
+
+function dublicateElements(array) {
+    let mass = [];
+    array.forEach((element) => {
+        mass.push(element, element);
+    });
+
+    return mass;
 }
